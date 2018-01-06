06ea5c6a215f2c3eca1b17212da5e3a5bbef8f68
Newhandnew <easytired@gmail.com>
2018-01-06 10:28:09 +0800

    make video from images

---
+++ make_video.py
@@ -0,0 +1,55 @@
+import cv2
+import os
+import argparse
+
+
+def getTargetPath(directory_path, start_filename):
+    filenames = os.listdir(directory_path)
+    filenames.sort()
+    filepaths = []
+    for file in filenames:
+        if file.startswith(start_filename):
+            filepaths.append(os.path.join(args.directory_path, file))
+    return filepaths
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process input filenames')
+    parser.add_argument('-d', '--directory_path', type=str, default='/home/new/Pictures/',
+                        help="full path to the images you want to merge")
+    parser.add_argument('-s', '--start_filename', type=str, default='street',
+                        help="starting string of file name")
+    parser.add_argument("-o", "--output", type=str, default='test',
+                        help="path to output video file")
+    parser.add_argument("-fps", '--fps', type=float, default=30.0,
+                        help="set fps for output video")
+    parser.add_argument("-change_size", '--change_size', type=bool, default=False,
+                        help="set size of output video")
+    parser.add_argument("-width", '--output_width', type=int, default=640,
+                        help="set width for output video")
+    parser.add_argument("-height", '--output_height', type=int, default=480,
+                        help="set height for output video")
+    args = parser.parse_args()
+
+    filepaths = getTargetPath(args.directory_path, args.start_filename)
+    print(filepaths)
+
+    output = args.output + '.avi'
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fps = args.fps
+    if args.change_size == False:
+        image = cv2.imread(filepaths[0])
+        height, width, _ = image.shape
+        size = (width, height)
+    else:
+    	size = (args.output_width, args.output_height)
+    print(size)
+    # size = (args.output_width, args.output_height)
+    videoWriter = cv2.VideoWriter(output, fourcc, fps, size)
+
+    for file in filepaths:
+        image = cv2.imread(file)
+        if args.change_size == True:
+        	image = cv2.resize(image, size)
+        videoWriter.write(image)
+
+    videoWriter.release()
